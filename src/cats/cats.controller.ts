262158ff9ba5dc1ab95a296bcbbfb43ee48a99c5
Ted Loungou <loungouviny15@gmail.com>
2023-05-28 19:02:13 +0200

    second commit learning nestjs

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,30 +1,36 @@
-import { Body, Controller, Get, HttpCode, Param, Post } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, Param, Post, Put } from "@nestjs/common";
 import { CatsService } from "./cats.service";
 import { Cat } from "./interfaces/cat.interface";
 import { CreateCatDto } from "./dto/create-cat.dto";
 import { Observable, of } from "rxjs";
 
 
-@Controller('cats')
+@Controller('/cats')
 export class CatsController {
     constructor(private catsService: CatsService) { };
 
     @Post()
     @HttpCode(201)
-    async create(@Body() createCatDto: CreateCatDto) {
+    create(@Body() createCatDto: CreateCatDto) {
         this.catsService.create(createCatDto);
         return `cat with name ${createCatDto.name} added`;
     }
 
     @Get(':id')
-    findOne(@Param() params: any) {
-        return this.catsService.findOne(params.id);
+    findOne(@Param('id') id: string): Observable<Cat> {
+        return of(this.catsService.findCatById(id));
     }
 
     @Get()
     findAll(): Observable<Cat[]> {
         return of(this.catsService.findAll());
     }
+
+    @Put(':id/:name/:age/:breed')
+    updateById(@Body() createCatDto: CreateCatDto) {
+        this.catsService.updateById(createCatDto);
+    }
+
 
 
 }
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -8,10 +8,29 @@ export class CatsService {
     create(cat: Cat) {
         this.cats.push(cat);
     }
-    findOne(id: string) {
-        return this.cats.find((item) => item.id === id);
+
+    updateById(catUpdated: Cat) {
+        try {
+            this.cats.find(item => item.id === catUpdated.id)!;
+
+        }
+        catch (err) {
+            console.error('Cat not found');
+        }
+        let cat: Cat = this.cats.find(item => item.id === catUpdated.id)!;
+        cat.name = catUpdated.name;
+        cat.age = catUpdated.age;
+        cat.breed = catUpdated.breed;
+        return this.cats.push(cat);
     }
+
     findAll(): Cat[] {
         return this.cats;
     }
+
+    findCatById(id: string): Cat {
+        return this.cats.find(item => item.id === id)!;
+
+    }
+
 } 
